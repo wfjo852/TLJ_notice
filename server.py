2a30f97ae60d3de3e0311fdb38cb67436c9b2ba3
박종호 <wfjo852@2cmonster.com>
2026-09-20 23:04:48 +0900

    최초 추가

---
+++ server.py
@@ -0,0 +1,10 @@
+﻿from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from functools import partial
+from pathlib import Path
+import os
+
+if __name__ == '__main__':
+    root = Path(__file__).resolve().parent / 'frontend'
+    server = ThreadingHTTPServer(('0.0.0.0', int(os.environ.get('PORT', '8000'))), partial(SimpleHTTPRequestHandler, directory=str(root)))
+    print('TLJ notice: http://localhost:8000', flush=True)
+    server.serve_forever()
